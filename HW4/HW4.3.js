e8d46e3d416b927e1067cddad5745753af22a554
cweta <semenchuksvetlana25@gmail.com>
2023-02-01 13:22:29 +0200

    4.3

--- HW4/HW4.3.js
+++ HW4/HW4.3.js
@@ -1,18 +1,22 @@
-function months() {
-    let browserLang = navigator.language; // забираем язык браузера который выставлен у тебя (англ, русский)
+class MonthException {
+    constructor(message) {
+      this.name = "MonthException";
+      this.message = message;
+    }
+  }
+  function months() {
+    let browserLang = navigatossssr.language;
     const month = Intl.DateTimeFormat(browserLang, { month: "long" });
-    return Array.from({ length: 12 }, (_, i) => month.format(new Date(0, 0).setMonth(i)));
-  } 
-
-
-
-
-
-  let MonthException= class {
-    construktor(message){
-        this.name=MonthException
-        const month = Intl.DateTimeFormat(browserLang, { month: "long" });
-    return Array.from({ length: 12 }, (_, i) => month.format(new Date(0, 0).setMonth(i)))
+    return Array.from({ length: 12 }, (_, i) =>
+      month.format(new Date(0, 0).setMonth(i))
+    );
+  }
+  function showMonthName(month) {
+    let monthName = this.months()[month - 1];
+    if (monthName) {
+      return monthName;
+    } else {
+      throw new MonthException("Incorrect month number");
     }
-       
-    }
+  }
+  showMonthName(3)
