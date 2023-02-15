afeb9b37b8177d8dd995ee5b8eb48bd09ac72549
cweta <semenchuksvetlana25@gmail.com>
2023-02-15 12:47:09 +0200

    all8

--- HW2/hw2.7.js
+++ HW2/hw2.7.js
@@ -1,1 +1,18 @@
+let data =  new Date()
+let timeNow = data.getHours()
 
+if (timeNow >= 23 || timeNow <= 5) {
+    alert("Good night!")
+} else {
+    if (timeNow >= 5 || timeNow <= 11) {
+        alert("Good morning!")
+    } else{
+        if ( timeNow >=11 || timeNow <= 17) {
+            alert("Good afternoon!")
+        } else{
+            if ( timeNow >= 17 || timeNow <=23) {
+                alert("Good evening!")
+            }
+        }
+    }
+}
