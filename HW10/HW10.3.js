befc71deec09f3516d1f01b1578071c8bd6e1e35
cweta <semenchuksvetlana25@gmail.com>
2023-02-21 22:40:48 +0200

    all 10

--- HW10/HW10.3.js
+++ HW10/HW10.3.js
@@ -1,9 +1,11 @@
 function mul(...manyMore) {
-  let result = 1
-  for( let i = 0; i< manyMore.lenght; i++){
-  if( typeof manyMore[i] === "number"){
-  console.log
+    let result = 1
+    for( let i = 0; i< manyMore.length; i++){
+    if( typeof manyMore[i] === 'number'){
+        result *= manyMore[i]
+    }
+    }
+    return result
   }
-  }
-  return result
-}
+  console.log(mul(1, "str", 2, 3, true))
+  console.log(mul(null, "str", false, true))
