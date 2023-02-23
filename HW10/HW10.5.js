e61f57758698a9d3bfe0070bee2b41458bd7ebfc
cweta <semenchuksvetlana25@gmail.com>
2023-02-23 18:22:31 +0200

    all 10.5

--- HW10/HW10.5.js
+++ HW10/HW10.5.js
@@ -1,9 +1,13 @@
 let keys = [1, 2, 3, 4];
 let values = ["div", "span", "b", "i"];
 
-let map = new Map()
+function mapBuilder(keys, values){
+let result = new Map()
 for( let i=0; i<keys.length; i++){
     map.set( keys[i], values[i])
+} 
+return result 
 }
+let map = mapBuilder(keys, values)
 console.log(map.size); // 4
 	console.log(map.get(2))
